bc04fd69df50d336f6d8ea2e91203e216c0505e1
BuildTools <unconfigured@null.spigotmc.org>
2022-12-19 23:21:02 +0200

    Created gem infuser

---
+++ src/main/java/com/str1llax/sf/block/GemInfuser.java
@@ -0,0 +1,9 @@
+package com.str1llax.sf.block;
+
+import net.minecraft.world.level.block.Block;
+
+public class GemInfuser extends Block {
+    public GemInfuser(Properties properties) {
+        super(properties);
+    }
+}
